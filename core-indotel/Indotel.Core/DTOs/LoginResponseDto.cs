618c01de621a8838b2d03a7cfd9155a181399023
meliodr <jarry.gonzalez02@gmail.com>
2026-07-09 05:29:26 -0400

    Agrega refresh token a respuesta login

--- core-indotel/Indotel.Core/DTOs/LoginResponseDto.cs
+++ core-indotel/Indotel.Core/DTOs/LoginResponseDto.cs
@@ -3,6 +3,8 @@ namespace Indotel.Core.DTOs;
 public class LoginResponseDto
 {
     public string Token { get; set; } = string.Empty;
+    public string RefreshToken { get; set; } = string.Empty;
     public DateTime ExpiraEn { get; set; }
+    public DateTime RefreshTokenExpiraEn { get; set; }
     public UsuarioSesionDto Usuario { get; set; } = new();
 }
