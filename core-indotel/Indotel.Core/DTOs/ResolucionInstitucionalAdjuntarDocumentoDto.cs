aef9bcd9b8b716b60a6dfc99a2d8313731d2680e
meliodr <jarry.gonzalez02@gmail.com>
2026-07-09 04:59:21 -0400

    Agrega DTO para adjuntar documento a resolucion

---
+++ core-indotel/Indotel.Core/DTOs/ResolucionInstitucionalAdjuntarDocumentoDto.cs
@@ -0,0 +1,7 @@
+namespace Indotel.Core.DTOs;
+
+public class ResolucionInstitucionalAdjuntarDocumentoDto
+{
+    public int? DocumentoReclamacionId { get; set; }
+    public string UrlDocumentoOficial { get; set; } = string.Empty;
+}
