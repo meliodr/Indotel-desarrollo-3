e65c024306124ded16f42a1dbde1a2c4585b63fe
meliodr <jarry.gonzalez02@gmail.com>
2026-07-08 21:14:49 -0400

    Agrega DTO crear ciudadano

---
+++ core-indotel/Indotel.Core/DTOs/CiudadanoCreateDto.cs
@@ -0,0 +1,11 @@
+namespace Indotel.Core.DTOs;
+
+public class CiudadanoCreateDto
+{
+    public string Cedula { get; set; } = string.Empty;
+    public string Nombres { get; set; } = string.Empty;
+    public string Apellidos { get; set; } = string.Empty;
+    public string Telefono { get; set; } = string.Empty;
+    public string Correo { get; set; } = string.Empty;
+    public string Direccion { get; set; } = string.Empty;
+}
