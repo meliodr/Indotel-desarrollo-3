9f36cb3dda236d4f2bc1a0812877f0429ad33756
meliodr <jarry.gonzalez02@gmail.com>
2026-07-09 04:04:16 -0400

    Agrega migraciones de resolucion cierre y auditoria

--- core-indotel/Indotel.Core/Migrations/20260709075623_Fase8AuditoriaInstitucional.Designer.cs
+++ core-indotel/Indotel.Core/Migrations/20260709075623_Fase8AuditoriaInstitucional.Designer.cs
@@ -4,6 +4,7 @@ using Indotel.Core.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Indotel.Core.Migrations
 {
     [DbContext(typeof(IndotelDbContext))]
-    partial class IndotelDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260709075623_Fase8AuditoriaInstitucional")]
+    partial class Fase8AuditoriaInstitucional
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
