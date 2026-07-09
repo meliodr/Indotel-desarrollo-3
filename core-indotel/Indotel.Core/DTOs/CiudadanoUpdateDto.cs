389f446ec32ae05eea4f3412318062ed5c2de973
meliodr <jarry.gonzalez02@gmail.com>
2026-07-08 22:57:11 -0400

    Agrega DTO para actualizar ciudadano

---
+++ core-indotel/Indotel.Core/DTOs/CiudadanoUpdateDto.cs
@@ -0,0 +1,10 @@
+namespace Indotel.Core.DTOs;
+
+public class CiudadanoUpdateDto
+{
+    public string Nombres { get; set; } = string.Empty;
+    public string Apellidos { get; set; } = string.Empty;
+    public string Telefono { get; set; } = string.Empty;
+    public string Correo { get; set; } = string.Empty;
+    public string Direccion { get; set; } = string.Empty;
+}
