3e219be690e790b357b371ba925401091e2e656f
meliodr <jarry.gonzalez02@gmail.com>
2026-07-08 21:19:51 -0400

    Agrega DTO cambio estado reclamacion

---
+++ core-indotel/Indotel.Core/DTOs/CambiarEstadoReclamacionDto.cs
@@ -0,0 +1,7 @@
+namespace Indotel.Core.DTOs;
+
+public class CambiarEstadoReclamacionDto
+{
+    public string EstadoNuevo { get; set; } = string.Empty;
+    public string Comentario { get; set; } = string.Empty;
+}
