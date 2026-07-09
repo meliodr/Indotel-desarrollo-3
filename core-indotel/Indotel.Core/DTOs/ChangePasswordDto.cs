d24d62631d46fc14e34e5e776a799f45054a9d3a
meliodr <jarry.gonzalez02@gmail.com>
2026-07-09 02:44:31 -0400

    Agrega DTO para cambio de clave propio

---
+++ core-indotel/Indotel.Core/DTOs/ChangePasswordDto.cs
@@ -0,0 +1,7 @@
+namespace Indotel.Core.DTOs;
+
+public class ChangePasswordDto
+{
+    public string PasswordActual { get; set; } = string.Empty;
+    public string PasswordNueva { get; set; } = string.Empty;
+}
