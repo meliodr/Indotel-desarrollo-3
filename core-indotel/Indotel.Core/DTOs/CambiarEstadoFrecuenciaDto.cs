2a7bba5fec0687b07fe40e33ddc17c32a7e67f03
meliodr <jarry.gonzalez02@gmail.com>
2026-07-09 05:11:47 -0400

    Agrega DTO cambiar estado frecuencia

---
+++ core-indotel/Indotel.Core/DTOs/CambiarEstadoFrecuenciaDto.cs
@@ -0,0 +1,7 @@
+namespace Indotel.Core.DTOs;
+
+public class CambiarEstadoFrecuenciaDto
+{
+    public string EstadoNuevo { get; set; } = string.Empty;
+    public string Observacion { get; set; } = string.Empty;
+}
