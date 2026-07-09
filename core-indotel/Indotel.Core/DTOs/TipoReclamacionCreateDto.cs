ac1e1f50079f036e1585cbb6a56d3170bc6e93e6
meliodr <jarry.gonzalez02@gmail.com>
2026-07-09 03:26:46 -0400

    Agrega DTO para tipo de reclamacion

---
+++ core-indotel/Indotel.Core/DTOs/TipoReclamacionCreateDto.cs
@@ -0,0 +1,7 @@
+namespace Indotel.Core.DTOs;
+
+public class TipoReclamacionCreateDto
+{
+    public string Nombre { get; set; } = string.Empty;
+    public string Descripcion { get; set; } = string.Empty;
+}
