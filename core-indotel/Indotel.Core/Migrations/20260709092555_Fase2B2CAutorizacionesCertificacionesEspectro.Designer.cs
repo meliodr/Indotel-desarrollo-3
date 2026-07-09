3d9b6d8d069e2f147a1b72c9a2955402660493f9
meliodr <jarry.gonzalez02@gmail.com>
2026-07-09 05:26:17 -0400

    Agrega migracion Fase 2B y 2C

--- core-indotel/Indotel.Core/Migrations/20260709092555_Fase2B2CAutorizacionesCertificacionesEspectro.Designer.cs
+++ core-indotel/Indotel.Core/Migrations/20260709092555_Fase2B2CAutorizacionesCertificacionesEspectro.Designer.cs
@@ -4,6 +4,7 @@ using Indotel.Core.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Indotel.Core.Migrations
 {
     [DbContext(typeof(IndotelDbContext))]
-    partial class IndotelDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260709092555_Fase2B2CAutorizacionesCertificacionesEspectro")]
+    partial class Fase2B2CAutorizacionesCertificacionesEspectro
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
