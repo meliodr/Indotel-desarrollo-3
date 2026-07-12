3557a384c7bd7defeee1af81130e9c43ed103b91
meliodr <jarry.gonzalez02@gmail.com>
2026-07-12 09:16:08 -0400

    Refuerza configuracion HTTP y cookie de la Web

--- INDOTEL.Web/Program.cs
+++ INDOTEL.Web/Program.cs
@@ -2,25 +2,29 @@ using Microsoft.AspNetCore.Authentication.Cookies;
 
 var builder = WebApplication.CreateBuilder(args);
 
-
 builder.Services.AddControllersWithViews();
-
 
 var apiBaseUrl = builder.Configuration.GetValue<string>("ApiSettings:BaseUrl");
 builder.Services.AddHttpClient("IndotelCore", client =>
 {
     client.BaseAddress = new Uri(apiBaseUrl ?? throw new InvalidOperationException("API Base URL no configurada."));
     client.DefaultRequestHeaders.Add("Accept", "application/json");
+    client.Timeout = TimeSpan.FromSeconds(30);
 });
-
 
 builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
     .AddCookie(options =>
     {
-        options.LoginPath = "/Auth/Login"; 
-        options.AccessDeniedPath = "/Auth/AccessDenied"; 
-        options.ExpireTimeSpan = TimeSpan.FromMinutes(20); 
+        options.LoginPath = "/Auth/Login";
+        options.AccessDeniedPath = "/Auth/AccessDenied";
+        options.ExpireTimeSpan = TimeSpan.FromMinutes(20);
+        options.SlidingExpiration = false;
+        options.Cookie.HttpOnly = true;
+        options.Cookie.SameSite = SameSiteMode.Lax;
+        options.Cookie.SecurePolicy = CookieSecurePolicy.SameAsRequest;
     });
+
+builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
@@ -32,10 +36,7 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
-
-
 app.UseAuthentication();
 app.UseAuthorization();
 
