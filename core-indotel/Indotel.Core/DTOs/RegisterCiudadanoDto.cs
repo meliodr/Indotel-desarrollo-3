910d0de44f9e0cca30dabe2df26eee9960f9aeee
meliodr <jarry.gonzalez02@gmail.com>
2026-07-13 08:44:03 -0400

    Sprint 1: valida registro ciudadano

--- core-indotel/Indotel.Core/DTOs/RegisterCiudadanoDto.cs
+++ core-indotel/Indotel.Core/DTOs/RegisterCiudadanoDto.cs
@@ -1,12 +1,69 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Indotel.Core.DTOs;
 
 public class RegisterCiudadanoDto
 {
-    public string Cedula { get; set; } = string.Empty;
-    public string Nombres { get; set; } = string.Empty;
-    public string Apellidos { get; set; } = string.Empty;
-    public string Telefono { get; set; } = string.Empty;
-    public string Correo { get; set; } = string.Empty;
-    public string Direccion { get; set; } = string.Empty;
-    public string Password { get; set; } = string.Empty;
+    private string _cedula = string.Empty;
+    private string _nombres = string.Empty;
+    private string _apellidos = string.Empty;
+    private string _telefono = string.Empty;
+    private string _correo = string.Empty;
+    private string _direccion = string.Empty;
+    private string _password = string.Empty;
+
+    [Required]
+    [StringLength(20)]
+    public string Cedula
+    {
+        get => _cedula;
+        set => _cedula = value ?? string.Empty;
+    }
+
+    [Required]
+    [StringLength(100)]
+    public string Nombres
+    {
+        get => _nombres;
+        set => _nombres = value ?? string.Empty;
+    }
+
+    [Required]
+    [StringLength(100)]
+    public string Apellidos
+    {
+        get => _apellidos;
+        set => _apellidos = value ?? string.Empty;
+    }
+
+    [StringLength(30)]
+    public string Telefono
+    {
+        get => _telefono;
+        set => _telefono = value ?? string.Empty;
+    }
+
+    [Required]
+    [EmailAddress]
+    [StringLength(254)]
+    public string Correo
+    {
+        get => _correo;
+        set => _correo = value ?? string.Empty;
+    }
+
+    [StringLength(300)]
+    public string Direccion
+    {
+        get => _direccion;
+        set => _direccion = value ?? string.Empty;
+    }
+
+    [Required]
+    [StringLength(200)]
+    public string Password
+    {
+        get => _password;
+        set => _password = value ?? string.Empty;
+    }
 }
