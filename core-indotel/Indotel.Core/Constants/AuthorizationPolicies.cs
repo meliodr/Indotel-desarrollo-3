b03ff43c92f57b49b2fbeedb2230136d184448fc
meliodr <jarry.gonzalez02@gmail.com>
2026-07-13 08:24:35 -0400

    Sprint 1: define políticas de autorización del Core

---
+++ core-indotel/Indotel.Core/Constants/AuthorizationPolicies.cs
@@ -0,0 +1,11 @@
+namespace Indotel.Core.Constants;
+
+public static class AuthorizationPolicies
+{
+    public const string CajaOperador = "CajaOperador";
+    public const string CajaConsulta = "CajaConsulta";
+    public const string AuditoriaLectura = "AuditoriaLectura";
+    public const string Administracion = "Administracion";
+    public const string Prestadora = "Prestadora";
+    public const string Ciudadano = "Ciudadano";
+}
