1abce9a040da3dfd92921f69b2421874f9d73f61
meliodr <jarry.gonzalez02@gmail.com>
2026-07-13 08:32:01 -0400

    Sprint 1: valida cambios de estado

--- core-indotel/Indotel.Core/DTOs/CambiarEstadoReclamacionDto.cs
+++ core-indotel/Indotel.Core/DTOs/CambiarEstadoReclamacionDto.cs
@@ -1,7 +1,24 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Indotel.Core.DTOs;
 
 public class CambiarEstadoReclamacionDto
 {
-    public string EstadoNuevo { get; set; } = string.Empty;
-    public string Comentario { get; set; } = string.Empty;
+    private string _estadoNuevo = string.Empty;
+    private string _comentario = string.Empty;
+
+    [Required]
+    [StringLength(80)]
+    public string EstadoNuevo
+    {
+        get => _estadoNuevo;
+        set => _estadoNuevo = value ?? string.Empty;
+    }
+
+    [StringLength(2000)]
+    public string Comentario
+    {
+        get => _comentario;
+        set => _comentario = value ?? string.Empty;
+    }
 }
