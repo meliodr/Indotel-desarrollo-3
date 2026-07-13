3fb7b1d162d6c80415184a2f27bdaa9f62a6b168
meliodr <jarry.gonzalez02@gmail.com>
2026-07-13 08:42:27 -0400

    Sprint 1: valida credenciales de login

--- core-indotel/Indotel.Core/DTOs/LoginRequestDto.cs
+++ core-indotel/Indotel.Core/DTOs/LoginRequestDto.cs
@@ -1,7 +1,26 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Indotel.Core.DTOs;
 
 public class LoginRequestDto
 {
-    public string Correo { get; set; } = string.Empty;
-    public string Password { get; set; } = string.Empty;
+    private string _correo = string.Empty;
+    private string _password = string.Empty;
+
+    [Required]
+    [EmailAddress]
+    [StringLength(254)]
+    public string Correo
+    {
+        get => _correo;
+        set => _correo = value ?? string.Empty;
+    }
+
+    [Required]
+    [StringLength(200)]
+    public string Password
+    {
+        get => _password;
+        set => _password = value ?? string.Empty;
+    }
 }
