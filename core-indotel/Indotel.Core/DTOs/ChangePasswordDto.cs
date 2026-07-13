d48231030f794471e728adbd2a10867194875776
meliodr <jarry.gonzalez02@gmail.com>
2026-07-13 08:42:49 -0400

    Sprint 1: valida cambio de contraseña

--- core-indotel/Indotel.Core/DTOs/ChangePasswordDto.cs
+++ core-indotel/Indotel.Core/DTOs/ChangePasswordDto.cs
@@ -1,7 +1,25 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Indotel.Core.DTOs;
 
 public class ChangePasswordDto
 {
-    public string PasswordActual { get; set; } = string.Empty;
-    public string PasswordNueva { get; set; } = string.Empty;
+    private string _passwordActual = string.Empty;
+    private string _passwordNueva = string.Empty;
+
+    [Required]
+    [StringLength(200)]
+    public string PasswordActual
+    {
+        get => _passwordActual;
+        set => _passwordActual = value ?? string.Empty;
+    }
+
+    [Required]
+    [StringLength(200)]
+    public string PasswordNueva
+    {
+        get => _passwordNueva;
+        set => _passwordNueva = value ?? string.Empty;
+    }
 }
